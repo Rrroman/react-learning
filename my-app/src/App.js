0a4debd19283bf12bc39deea34f7a45dc741f85b
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-07 01:10:03 +0200

    init: my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="app__title">Hello there!</div>
+      </div>
+    );
+  }
+}
+
+export default App;
