d0a5054b005fa58841090ec68b51c9bdb0d7be36
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-07 07:21:32 +0200

    feat: create functional component

---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = () => {
+  return (
+    <h2>
+      Person is here and my age is {Math.floor(Math.random() * 30)} and it is
+      magical just reload the page.
+    </h2>
+  );
+};
+
+export default person;
