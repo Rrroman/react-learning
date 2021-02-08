053ec333ea4bbd0284f22f1fe504c075046b609e
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-08 21:37:33 +0200

    feat: using state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 25 },
+      { name: 'Doe', age: 40 },
+      { name: 'Jill', age: 30 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <div className="app__title">Hello there!</div>
-        <Person name="Roman" age="31" />
-        <Person name="Max" age="28">
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My hobby is chess.
         </Person>
-        <Person name="Julia" age="21" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
