ed4884ca86b14b8f114f62eb109e38e5c4111df7
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-10 22:46:29 +0200

    feat: made list of persons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
 
   toggleNameHandler = () => {
     const doesShow = this.state.showNames;
+
     this.setState({
       showNames: !doesShow,
     });
@@ -72,23 +73,9 @@ class App extends Component {
     if (this.state.showNames) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={this.switchNameHandler.bind(this, 'Roman')}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            inputNameChange={this.inputNameChangeHandler}
-            selectInput={this.selectInputHandler}
-          >
-            My hobby is chess.
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
