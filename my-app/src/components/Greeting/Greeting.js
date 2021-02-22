da2c275b4ec198f4048c0c100d8705da1e92e3ff
Roman Kohutyak <romankohutyak@gmail.com>
2021-02-22 14:44:03 +0200

    feat: ref with React Hook

--- my-app/src/components/Greeting/Greeting.js
+++ my-app/src/components/Greeting/Greeting.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Greeting.module.css';
 
 const Greeting = (props) => {
+  const toggleButtonRef = useRef(null);
+
   useEffect(() => {
     console.log('Greeting.js -> useEffecting...');
-    setTimeout(() => {
-      alert('Saved data to cloud!');
-    }, 1000);
+    // setTimeout(() => {
+    //   alert('Saved data to cloud!');
+    // }, 1000);
+
+    toggleButtonRef.current.click();
     return () => {
       console.log('Greeting.js -> clean Up work in useEffect');
     };
@@ -22,7 +26,11 @@ const Greeting = (props) => {
   return (
     <div>
       <div className={classes.greeting__title}>{props.appTitle}</div>
-      <button className={classes.greeting__button} onClick={props.clicked}>
+      <button
+        className={classes.greeting__button}
+        onClick={props.clicked}
+        ref={toggleButtonRef}
+      >
         Toggle names
       </button>
     </div>
